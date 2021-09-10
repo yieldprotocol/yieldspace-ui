91b41160d44758e4c8bd24ef9372b0f616347ed4
Ben DiFrancesco <ben@scopelift.co>
2021-09-10 16:53:47 -0400

    Update ReimbursementToken to inherit from ERC20Permit

--- contracts/contracts/ReimbursementToken.sol
+++ contracts/contracts/ReimbursementToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.7;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract ReimbursementToken is ERC20 {
+contract ReimbursementToken is ERC20Permit {
     uint256 public immutable maturity;
     address public immutable underlying;
 
@@ -13,7 +13,7 @@ contract ReimbursementToken is ERC20 {
         uint256 _maturity,
         address _underlying,
         uint256 _supply,
-        address _receiver) ERC20(_name, _symbol)
+        address _receiver) ERC20Permit(_name) ERC20(_name, _symbol)
     {
         require(_maturity > block.timestamp, "ReimbursementToken: Maturity date must be in future");
         maturity = _maturity;
