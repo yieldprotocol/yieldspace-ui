c3b051a390b452f9569133c34b6c689be76730fd
marcomariscal <marco.a.mariscal@gmail.com>
2022-01-28 17:47:31 -0500

    fix nav with relevant paths

--- state/actions/chain.ts
+++ state/actions/chain.ts
@@ -1,5 +1,5 @@
-import { ActionType } from 'state/actionTypes/chain';
-import { IChainConnection, IChainConnectionAction, IChainResetConnectionAction } from 'state/types/chain';
+import { ActionType } from '../actionTypes/chain';
+import { IChainConnection, IChainConnectionAction, IChainResetConnectionAction } from '../types/chain';
 
 export const updateConnection = (connection: IChainConnection): IChainConnectionAction => ({
   type: ActionType.CONNECTION,
