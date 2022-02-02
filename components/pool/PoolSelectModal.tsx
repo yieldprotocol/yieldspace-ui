5b5fa58a938ca26c16c94351cd24906983aa4513
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-02 16:04:03 -0500

    update colors for pools in pool select modal

--- components/pool/PoolSelectModal.tsx
+++ components/pool/PoolSelectModal.tsx
@@ -3,9 +3,8 @@ import tw from 'tailwind-styled-components';
 import { IPool, IPoolMap } from '../../lib/protocol/types';
 import AssetLogo from '../common/AssetLogo';
 import Modal from '../common/Modal';
-import StackedAssetLogos from '../common/StackedAssetLogos';
 
-const Button = tw.button`bg-gray-700 hover:bg-gray-500/25 border-2 border-gray-700 hover:border-secondary-500 text-gray-50`;
+const Button = tw.button` border-2 border-gray-700 hover:border-secondary-500 text-gray-900`;
 
 interface IPoolSelectModal {
   pools: IPoolMap;
@@ -14,23 +13,27 @@ interface IPoolSelectModal {
   action: (pool: IPool) => void;
 }
 
-const PoolSelectModal: FC<IPoolSelectModal> = ({ pools, open, setOpen, action }) => (
-  <Modal isOpen={open} setIsOpen={setOpen}>
-    {Object.values(pools).map((pool) => (
-      <Button
-        className="flex my-2 p-4 bg-gray-700 rounded-md gap-3 align-middle w-full items-center"
-        onClick={() => {
-          action(pool);
-          setOpen(false);
-        }}
-        key={pool.address}
-      >
-        {/* <StackedAssetLogos image1={'eth'} image2={'usdc'} /> */}
-        <AssetLogo image="usdc" />
-        {pool.name}
-      </Button>
-    ))}
-  </Modal>
-);
+const PoolSelectModal: FC<IPoolSelectModal> = ({ pools, open, setOpen, action }) => {
+  return (
+    <Modal isOpen={open} setIsOpen={setOpen}>
+      {Object.values(pools).map((pool) => (
+        <Button
+          className="flex my-2 p-4 rounded-md gap-3 align-middle w-full items-center hover:opacity-80"
+          style={{
+            background: pool.color,
+          }}
+          onClick={() => {
+            action(pool);
+            setOpen(false);
+          }}
+          key={pool.address}
+        >
+          <AssetLogo image="usdc" />
+          {pool.name}
+        </Button>
+      ))}
+    </Modal>
+  );
+};
 
 export default PoolSelectModal;
