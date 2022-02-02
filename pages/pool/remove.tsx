376a59553c265e1de7840e3c8b40e0736e2b12b2
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-02 11:22:51 -0500

    add remove liquidity view

---
+++ pages/pool/remove.tsx
@@ -0,0 +1,14 @@
+import tw from 'tailwind-styled-components';
+import RemoveLiquidity from '../../components/pool/RemoveLiquidity';
+
+const Container = tw.div`p-10 text-center align-middle justify-center`;
+
+const Create = () => {
+  return (
+    <Container>
+      <RemoveLiquidity />
+    </Container>
+  );
+};
+
+export default Create;
