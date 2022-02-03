35957916c6448fb41507d25d29e33ad6728b234e
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-03 13:52:24 -0500

    fix path

--- pages/trade.tsx
+++ pages/trade.tsx
@@ -1,5 +1,5 @@
 import tw from 'tailwind-styled-components';
-import TradeWidget from '../components/Trade/TradeWidget';
+import TradeWidget from '../components/trade/TradeWidget';
 
 const Container = tw.div`text-center align-middle justify-center`;
 
