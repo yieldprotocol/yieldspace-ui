42d92cdb78f85c7764c4af978953e6f0d0e7cbdd
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-07 10:20:08 -0500

    remove default provider methodology

--- hooks/useConnector.ts
+++ hooks/useConnector.ts
@@ -1,10 +1,6 @@
-import { Web3Provider } from '@ethersproject/providers';
-import { getPriorityConnector, Web3ReactHooks } from '@web3-react/core';
+import { getPriorityConnector } from '@web3-react/core';
 import { MetaMask } from '@web3-react/metamask';
 import type { Connector } from '@web3-react/types';
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
-import { CHAINS } from '../config/chains';
 // import { WalletConnect } from '@web3-react/walletconnect'
 import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask';
 // import { hooks as walletConnectHooks, walletConnect } from '../connectors/walletConnect'
@@ -28,18 +24,7 @@ const useConnector = () => {
   const provider = usePriorityProvider();
   const ensName = usePriorityENSName(provider);
 
-  // useEffect(() => {
-  //   if (!provider) {
-  //     const defaultProvider = getDefaultProvider();
-  //     setConnectorState({ chainId: 1, account: null, ensName: null, connector: null, provider: defaultProvider });
-  //   } else {
-  //     setConnectorState({ chainId, account, ensName, connector, provider });
-  //   }
-  // }, [provider, chainId, account, connector, ensName]);
-
   return { chainId, account, connector, provider, ensName };
 };
-
-const getDefaultProvider = (chainId: number) => new ethers.providers.JsonRpcProvider(CHAINS[chainId].urls[0]);
 
 export default useConnector;
