d4f173c39902da2f564d9b0bb9dac63f8dd0ae03
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-07 10:19:51 -0500

    no account verbiage

--- components/pool/Pools.tsx
+++ components/pool/Pools.tsx
@@ -1,17 +1,23 @@
 import { FC } from 'react';
 import tw from 'tailwind-styled-components';
+import useConnector from '../../hooks/useConnector';
 import PoolItem from './PoolListItem';
 
 const Container = tw.div`p-2`;
 
 interface IPools {}
 
-const Pools: FC<IPools> = () => (
-  <Container>
-    <PoolItem />
-    <PoolItem />
-    <PoolItem />
-  </Container>
-);
+const Pools: FC<IPools> = () => {
+  const { account } = useConnector();
+
+  if (!account) return <Container>Please connect to your wallet</Container>;
+  return (
+    <Container>
+      <PoolItem />
+      <PoolItem />
+      <PoolItem />
+    </Container>
+  );
+};
 
 export default Pools;
