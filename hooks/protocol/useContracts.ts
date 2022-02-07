9bc591e13f5fcabda87d5b3618533a77474fe32a
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-07 10:20:19 -0500

    stricter types

--- hooks/protocol/useContracts.ts
+++ hooks/protocol/useContracts.ts
@@ -1,14 +1,9 @@
 import { CAULDRON, LADLE } from '../../constants';
-import { ethers } from 'ethers';
-import { Web3Provider } from '@ethersproject/providers';
 import { getContracts } from '../../lib/protocol';
-import { IContractMap } from '../../lib/protocol/types';
+import { Provider } from '../../lib/protocol/types';
 
 export const CONTRACTS_TO_FETCH = [LADLE, CAULDRON];
 
-const useContracts = (
-  provider: ethers.providers.JsonRpcProvider | Web3Provider,
-  chainId: number
-): IContractMap | undefined => getContracts(provider, chainId);
+const useContracts = (provider: Provider, chainId: number) => getContracts(provider, chainId);
 
 export default useContracts;
