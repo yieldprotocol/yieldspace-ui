e524c9b0d84fc3bddd7bb1d0b1fc4c9019e5c1c5
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-14 10:03:18 -0500

    style

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,22 +1,17 @@
 import { FC } from 'react';
 import tw from 'tailwind-styled-components';
-// const OuterStyle = tw.div`h-full w-full justify-center dark:bg-secondary-500/25 bg-secondary-600/50 align-middle px-4 py-2 dark:text-secondary-500 text-gray-50 rounded-md dark:hover:bg-secondary-600/25 hover:bg-secondary-500/50`;
-const InnerStyle = tw.button`h-full w-full justify-center dark:bg-secondary-500/25 bg-secondary-600/50 align-middle px-4 py-2 dark:text-secondary-500 text-gray-50 rounded-md dark:hover:bg-secondary-600/25 hover:bg-secondary-500/50`;
-const Outer = tw.div`h-full w-full rounded-xl  bg-secondary-600 hover:opacity-80`;
-const Inner = tw.button`h-full w-full px-4 py-2.5 dark:text-gray-50 text-gray-50 rounded-lg`;
+
+const Style = tw.button`bg-primary-700 h-full w-full px-4 py-2.5 dark:text-gray-50 text-gray-50 rounded-lg`;
+
 interface IButton {
   action: () => void;
   disabled?: boolean;
 }
-const Button: FC<IButton> = ({ action, disabled, children }) => (
-  <Outer
 
-  // style={{ background: `linear-gradient(101.3deg,#ff8320 26.97%,#48c295)` }}
-  >
-    <Inner onClick={action} disabled={disabled}>
-      {children}
-    </Inner>
-  </Outer>
+const Button: FC<IButton> = ({ action, disabled, children }) => (
+  <Style onClick={action} disabled={disabled}>
+    {children}
+  </Style>
 );
 
 export default Button;
