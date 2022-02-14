260d519badb55082b103bc20caeb328bde3b48ae
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-14 10:14:07 -0500

    style

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import tw from 'tailwind-styled-components';
 
-const Style = tw.button`bg-primary-700 h-full w-full px-4 py-2.5 dark:text-gray-50 text-gray-50 rounded-lg`;
+const Style = tw.button`bg-primary-700 h-full w-full px-4 py-2.5 dark:text-gray-50 text-gray-50 rounded-lg hover:opacity-80`;
 
 interface IButton {
   action: () => void;
