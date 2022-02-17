d8cd8b18b4bbfc10040b86a72b2d0ffa71133cd1
marcomariscal <marco.a.mariscal@gmail.com>
2022-02-17 18:17:00 -0500

    update specific pool view with data

--- components/pool/PoolItem.tsx
+++ components/pool/PoolItem.tsx
@@ -1,14 +1,35 @@
+import { FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import tw from 'tailwind-styled-components';
+import { IPool } from '../../lib/protocol/types';
 import Button from '../common/Button';
 import { BorderWrap } from '../styles';
+import usePools from '../../hooks/protocol/usePools';
 
 const Inner = tw.div`align-middle text-left p-5`;
 const ButtonWrap = tw.div`flex justify-between gap-10`;
 const PoolData = tw.div`h-20 align-middle text-center mt-5`;
 
-const PoolItem = () => {
+interface IPoolItem {
+  pool: IPool;
+}
+
+const PoolItem: FC<IPoolItem> = () => {
   const router = useRouter();
+  const { data: pools } = usePools();
+  const { address } = router.query;
+
+  const [pool, setPool] = useState<IPool | undefined>();
+
+  useEffect(() => {
+    if (pools) {
+      const _pool = pools[address as string];
+      _pool && setPool(_pool);
+    }
+  }, [pools, address]);
+
+  if (!pool) return null;
+
   return (
     <BorderWrap>
       <Inner>
@@ -16,7 +37,7 @@ const PoolItem = () => {
           <Button action={() => router.push('/pool/add')}>Add Liquidity</Button>
           <Button action={() => router.push('/pool/remove')}>Remove</Button>
         </ButtonWrap>
-        <PoolData>some pool data and user balances</PoolData>
+        <PoolData>lp tokens: {pool.lpTokenBalance_}</PoolData>
       </Inner>
     </BorderWrap>
   );
--- components/pool/PoolListItem.tsx
+++ components/pool/PoolListItem.tsx
@@ -1,27 +1,26 @@
 import Link from 'next/link';
 import { FC } from 'react';
 import tw from 'tailwind-styled-components';
+import { IPool } from '../../lib/protocol/types';
 
 const Container = tw.button`w-full my-1.5 dark:hover:bg-gray-700/50 hover:bg-gray-400/50 dark:bg-gray-800/80 bg-gray-300 rounded-md shadow-md`;
 const Inner = tw.div`align-middle text-left p-5`;
 
-interface IPoolItem {}
+interface IPoolListItem {
+  pool: IPool;
+}
 
-const PoolListItem: FC<IPoolItem> = () => {
-  const poolId = 1;
-  return (
-    <Link href={`/pool/${poolId}`} passHref>
-      <Container>
-        <Inner>
-          <div className="font-bold">fyToken/Token Pool</div>
-          <div className="flex gap-5 mt-3">
-            <div className="text-sm">fyToken: balance</div>
-            <div className="text-sm">token: balance</div>
-          </div>
-        </Inner>
-      </Container>
-    </Link>
-  );
-};
+const PoolListItem: FC<IPoolListItem> = ({ pool }) => (
+  <Link href={`/pool/${pool.address}`} passHref>
+    <Container>
+      <Inner>
+        <div className="font-bold">{pool.name}</div>
+        <div className="flex gap-5 mt-3">
+          <div className="text-sm">LP Token Balance: {pool.lpTokenBalance_}</div>
+        </div>
+      </Inner>
+    </Container>
+  </Link>
+);
 
 export default PoolListItem;
