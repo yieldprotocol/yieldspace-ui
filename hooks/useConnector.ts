a5f199d97a6274aca45a4066b721152db98509ab
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-03 17:01:58 -0500

    remove unused func

--- hooks/useConnector.ts
+++ hooks/useConnector.ts
@@ -1,17 +1,11 @@
 import { getPriorityConnector } from '@web3-react/core';
 import { MetaMask } from '@web3-react/metamask';
-import type { Connector } from '@web3-react/types';
 // import { WalletConnect } from '@web3-react/walletconnect'
 import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask';
 // import { hooks as walletConnectHooks, walletConnect } from '../connectors/walletConnect'
 
-function getName(connector: Connector) {
-  if (connector instanceof MetaMask) return 'MetaMask';
-  //   if (connector instanceof WalletConnect) return 'WalletConnect'
-  return 'Unknown';
-}
-
 const useConnector = () => {
+  console.log('fetching connector');
   const { usePriorityConnector, usePriorityChainId, usePriorityAccount, usePriorityENSName, usePriorityProvider } =
     getPriorityConnector(
       [metaMask as MetaMask, metaMaskHooks]
