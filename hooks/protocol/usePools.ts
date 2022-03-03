cea1ec70d978223b287da9b6419dce2725f56bfb
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-03 12:03:24 -0500

    update with chain id

--- hooks/protocol/usePools.ts
+++ hooks/protocol/usePools.ts
@@ -9,7 +9,7 @@ const usePools = () => {
   const chainIdToUse = chainId ?? DEFAULT_CHAIN_ID;
   // const provider = useDefaultProvider(chainIdToUse);
   const contractMap = useContracts(provider!, chainIdToUse);
-  const { data, error } = useSWR([provider, contractMap, account], getPools);
+  const { data, error } = useSWR([provider, contractMap, chainIdToUse, account], getPools);
   return { data, loading: !data && !error, error };
 };
 
