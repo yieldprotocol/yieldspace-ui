5f0a98709ce3d44ff1eaf28359aeb7c53c3dcb7d
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-03 17:02:23 -0500

    memoize contracts

--- hooks/protocol/useContracts.ts
+++ hooks/protocol/useContracts.ts
@@ -1,11 +1,16 @@
 import { useMemo } from 'react';
 import { CAULDRON, LADLE } from '../../constants';
 import { getContracts } from '../../lib/protocol';
-import { Provider } from '../../lib/protocol/types';
+import useDefaultProvider from '../useDefaultProvider';
 
 export const CONTRACTS_TO_FETCH = [LADLE, CAULDRON];
 
-const useContracts = (provider: Provider, chainId: number) =>
-  useMemo(() => getContracts(provider, chainId), [provider, chainId]);
+const useContracts = (chainId: number) => {
+  const provider = useDefaultProvider(chainId);
+  return useMemo(() => {
+    console.log('fetching contracts');
+    return getContracts(provider!, chainId);
+  }, [provider, chainId]);
+};
 
 export default useContracts;
