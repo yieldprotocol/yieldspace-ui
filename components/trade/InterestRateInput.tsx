ca22436985ca902c0dddcd77141c725fb14507f4
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-07 13:27:34 -0500

    fix text color

--- components/trade/InterestRateInput.tsx
+++ components/trade/InterestRateInput.tsx
@@ -6,7 +6,7 @@ interface IInterestRateInput {
   setRate: (rate: string) => void;
   disabled?: boolean;
 }
-const Input = tw.input`caret-gray-800 dark:caret-gray-50 text-2xl rounded-md appearance-none w-full dark:bg-gray-900 bg-gray-100 dark:focus:text-gray-50 focus:text-gray-800 text-gray-300 py-3 px-4 leading-tight focus:outline-none`;
+const Input = tw.input`caret-gray-800 dark:caret-gray-50 text-2xl rounded-md appearance-none w-full dark:bg-gray-900 bg-gray-100 dark:focus:text-gray-50 focus:text-gray-800 dark:text-gray-300 text-gray-700 py-3 px-4 leading-tight focus:outline-none`;
 
 const InterestRateInput: FC<IInterestRateInput> = ({ rate, setRate, disabled = false }) => (
   <div className="flex justify-between items-center">
