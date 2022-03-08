954dd8f1002df3e669fda077298d2e64977f7221
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-08 17:03:41 -0500

    centering

--- components/pool/RemoveConfirmation.tsx
+++ components/pool/RemoveConfirmation.tsx
@@ -8,7 +8,7 @@ import { IRemoveLiquidityForm } from './RemoveLiquidity';
 
 const Container = tw.div`relative flex justify-center items-center w-full`;
 const Wrap = tw.div`w-full text-center text-lg align-middle items-center`;
-const InputStyleContainer = tw.div`flex rounded-md justify-between p-1 w-full gap-5 align-middle border dark:border-gray-800 dark:bg-gray-800 bg-gray-300 border-gray-300`;
+const InputStyleContainer = tw.div`flex rounded-md justify-between p-1 w-full gap-5 align-middle border dark:border-gray-800 dark:bg-gray-800 bg-gray-300 border-gray-300 items-center`;
 const InputsOuter = tw.div`flex items-center justify-center relative w-full`;
 const InputStyle = tw.div`h-full caret-gray-800 dark:caret-gray-50 text-2xl appearance-none w-full dark:bg-gray-800 bg-gray-300 dark:focus:text-gray-50 focus:text-gray-800 dark:text-gray-300 text-gray-800 py-1 px-4 leading-tight focus:outline-none `;
 const InputInner = tw.div`w-auto ml-3 text-center text-lg align-middle my-1 items-center`;
