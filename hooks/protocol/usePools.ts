808a2e2b22cea74517e541859351cd56e2121a7b
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-14 15:11:42 -0400

    add swr options to prevent revalidate on focus

--- hooks/protocol/usePools.ts
+++ hooks/protocol/usePools.ts
@@ -1,4 +1,4 @@
-import useSWR from 'swr';
+import useSWR, { SWRConfiguration } from 'swr';
 import { getPools } from '../../lib/protocol';
 import useConnector from '../useConnector';
 import useContracts from './useContracts';
@@ -7,7 +7,8 @@ const usePools = () => {
   const { chainId, account, provider } = useConnector();
   const contractMap = useContracts();
 
-  const { data, error } = useSWR([provider, contractMap, chainId, account], getPools);
+  const options = { revalidateOnFocus: false } as SWRConfiguration;
+  const { data, error } = useSWR([provider, contractMap, chainId, account], getPools, options);
 
   return { data, loading: !data && !error, error };
 };
