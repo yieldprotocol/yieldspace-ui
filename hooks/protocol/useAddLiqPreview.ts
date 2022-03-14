1d52d351d9b4e9204cd4030d37b04e80f07c9b7b
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-14 15:08:56 -0400

    refactor logic to minimize deps

--- hooks/protocol/useAddLiqPreview.ts
+++ hooks/protocol/useAddLiqPreview.ts
@@ -2,16 +2,17 @@ import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
 import { AddLiquidityActions } from '../../lib/protocol/liquidity/types';
 import { IPool } from '../../lib/protocol/types';
+import { cleanValue } from '../../utils/appUtils';
 import { fyTokenForMint } from '../../utils/yieldMath';
 
 const useAddLiqPreview = (pool: IPool, baseAmount: string, method: AddLiquidityActions, slippageTolerance = 0.001) => {
   const [lpTokenPreview, setLpTokenPreview] = useState<string>();
-  const { totalSupply, decimals, contract, getTimeTillMaturity, ts, g1 } = pool;
-
-  const _baseAmount = ethers.utils.parseUnits(baseAmount, decimals);
 
   useEffect(() => {
     const getPrevewData = async () => {
+      const { totalSupply, decimals, contract, getTimeTillMaturity, ts, g1 } = pool;
+
+      const _baseAmount = ethers.utils.parseUnits(baseAmount, decimals);
       const [cachedBaseReserves, cachedFyTokenReserves] = await contract.getCache();
       const cachedRealReserves = cachedFyTokenReserves.sub(totalSupply);
 
@@ -31,11 +32,11 @@ const useAddLiqPreview = (pool: IPool, baseAmount: string, method: AddLiquidityA
         .mul(_fyTokenToBuy.add(method === AddLiquidityActions.MINT_WITH_BASE ? ethers.constants.Zero : _baseAmount)) // use base amount which is equal to the fyToken amount provided to the pool
         .div(cachedRealReserves.sub(_fyTokenToBuy));
       const lpTokenPreview_ = ethers.utils.formatUnits(tokensMinted, decimals);
-      setLpTokenPreview(lpTokenPreview_);
+      setLpTokenPreview(cleanValue(lpTokenPreview_, 2));
     };
 
     getPrevewData();
-  }, [_baseAmount, contract, decimals, g1, getTimeTillMaturity, method, slippageTolerance, totalSupply, ts]);
+  }, [baseAmount, method, pool, slippageTolerance]);
 
   return { lpTokenPreview };
 };
