cd0c9e7ab2bf2ef0c7a48dd475c555b29edccac3
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-15 17:02:09 -0400

    use tailwind styling for toasty

--- components/common/Toasty.tsx
+++ components/common/Toasty.tsx
@@ -5,12 +5,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Toasty = () => {
   const { theme } = useColorTheme();
+
   return (
     <ToastContainer
+      toastClassName={() =>
+        'bg-gray-300 dark:bg-gray-800 text-gray-800 dark:text-gray-50 relative flex p-2 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer'
+      }
+      bodyClassName={() => 'flex text-md font-med p-3'}
       position="bottom-right"
       pauseOnHover
       closeOnClick
-      toastStyle={{ background: theme === 'light' ? '#e4e4e7' : '#18181b' }}
+      // toastStyle={{ background: theme === 'light' ? '#e4e4e7' : '#18181b' }}
       closeButton={({ closeToast }) => (
         <XIcon height="1rem" width="1rem" color={theme === 'dark' ? '#e4e4e7' : '#18181b'} onClick={closeToast} />
       )}
