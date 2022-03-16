7cdfee7cf31ef5f9d41331a3670c9797bae64eda
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-16 18:26:00 -0400

    fix type

--- hooks/protocol/useAddLiqPreview.ts
+++ hooks/protocol/useAddLiqPreview.ts
@@ -24,7 +24,7 @@ const useAddLiqPreview = (pool: IPool, baseAmount: string, method: AddLiquidityA
         cachedRealReserves,
         cachedFyTokenReserves,
         _baseAmount,
-        getTimeTillMaturity(),
+        getTimeTillMaturity().toString(),
         ts,
         g1,
         decimals,
