808aeccef41c578903dd575edbe67357e8f6b431
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-18 12:12:18 -0400

    add link prop

--- hooks/useToasty.tsx
+++ hooks/useToasty.tsx
@@ -6,45 +6,54 @@ const Body = tw.div`flex gap-3 p-2 items-center align-middle`;
 const Text = tw.div`text-sm`;
 const Spinner = tw.div`spinner-border animate-spin h-5 w-5 border-5 rounded-full border-primary-200 border-t-secondary-400`;
 
-const Pending = ({ msg }: { msg: string }) => (
+const Inner = ({ msg, link = undefined }: { msg: string; link: string | undefined }) =>
+  link ? (
+    <a href={link}>
+      <Text>{msg}</Text>
+    </a>
+  ) : (
+    <Text>{msg}</Text>
+  );
+
+const Pending = ({ msg, link = undefined }: { msg: string; link: string | undefined }) => (
   <Body>
     {/* <Spinner /> */}
-    <Text>{msg}</Text>
+    <Inner msg={msg} link={link} />
   </Body>
 );
 
-const Success = ({ msg }: { msg: string }) => (
+const Success = ({ msg, link = undefined }: { msg: string; link: string | undefined }) => (
   <Body>
-    <CheckCircleIcon color="#10b981" height="30px" width="30px" />
-    <Text>{msg}</Text>
+    <CheckCircleIcon color="#10b981" height="25px" width="25px" />
+    <Inner msg={msg} link={link} />
   </Body>
 );
 
-const Error = ({ msg }: { msg: string }) => (
+const Error = ({ msg, link }: { msg: string; link: string | undefined }) => (
   <Body>
-    <ExclamationCircleIcon color="#ef4444" height="30px" width="30px" />
-    <Text>{msg}</Text>
+    <ExclamationCircleIcon color="#ef4444" height="25px" width="25px" />
+    <Inner msg={msg} link={link} />
   </Body>
 );
 
 const useToasty = () => {
-  const toasty = (promise: () => Promise<void>, msg: string) => {
+  const toasty = (promise: () => Promise<void>, msg: string, link: string | undefined) => {
     toast.promise(promise, {
       pending: {
         render() {
-          return <Pending msg={msg} />;
+          return <Pending msg={msg} link={link} />;
         },
         icon: true,
       },
       success: {
         render() {
-          return <Success msg={msg} />;
+          return <Success msg={msg} link={link} />;
         },
         icon: false,
       },
       error: {
         render() {
-          return <Error msg={msg} />;
+          return <Error msg={msg} link={link} />;
         },
         icon: false,
       },
