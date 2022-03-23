d076ccee9854d3a730b3ebda9f126e2c29c6375f
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-23 17:48:22 -0400

    remove unnecessary

--- hooks/protocol/useRemoveLiquidity.ts
+++ hooks/protocol/useRemoveLiquidity.ts
@@ -1,12 +1,13 @@
-import { BigNumberish, ethers } from 'ethers';
+import { ethers } from 'ethers';
 import { RemoveLiquidityActions } from '../../lib/protocol/liquidity/types';
 import { IPool } from '../../lib/protocol/types';
 import { cleanValue } from '../../utils/appUtils';
-import { burn, calcPoolRatios } from '../../utils/yieldMath';
+import { calcPoolRatios } from '../../utils/yieldMath';
 import useConnector from '../useConnector';
 import useSignature from '../useSignature';
 import useLadle from './useLadle';
 import useTransaction from '../useTransaction';
+import { LadleActions } from '../../lib/tx/operations';
 
 export const useRemoveLiquidity = (pool: IPool, input: string, method: RemoveLiquidityActions, description: string) => {
   const { account } = useConnector();
@@ -14,27 +15,15 @@ export const useRemoveLiquidity = (pool: IPool, input: string, method: RemoveLiq
   const { transact, isTransacting, txSubmitted } = useTransaction();
   const { ladleContract, forwardPermitAction, batch, transferAction, burnForBaseAction, burnAction } = useLadle();
 
-  // settings
-  const approveMax = false;
-  const slippageTolerance = 0.001;
-
   const removeLiquidity = async () => {
     if (!pool) throw new Error('no pool'); // prohibit trade if there is no pool
 
-    const { base, fyToken } = pool;
+    const { base } = pool;
     const cleanInput = cleanValue(input, base.decimals);
     const _input = ethers.utils.parseUnits(cleanInput, base.decimals);
-    const _inputLessSlippage = _input;
 
     const [cachedBaseReserves, cachedFyTokenReserves] = await pool.contract.getCache();
     const cachedRealReserves = cachedFyTokenReserves.sub(pool.totalSupply);
-
-    const [_baseTokenReceived, _fyTokenReceived] = burn(
-      pool.baseReserves,
-      cachedRealReserves,
-      pool.totalSupply,
-      _inputLessSlippage
-    );
 
     const [minRatio, maxRatio] = calcPoolRatios(cachedBaseReserves, cachedRealReserves);
 
@@ -53,19 +42,12 @@ export const useRemoveLiquidity = (pool: IPool, input: string, method: RemoveLiq
           ignoreIf: alreadyApproved,
         },
       ]);
-      const [, , , deadline, v, r, s] = permits[0].args!;
+
+      const [, , , deadline, v, r, s] = permits[0].args! as LadleActions.Args.FORWARD_PERMIT;
 
       return batch(
         [
-          forwardPermitAction(
-            pool.address,
-            ladleContract?.address!,
-            _input,
-            deadline as BigNumberish,
-            v as BigNumberish,
-            r as Buffer,
-            s as Buffer
-          )!,
+          forwardPermitAction(pool.address, ladleContract?.address!, _input, deadline, v, r, s)!,
           transferAction(pool.address, pool.address, _input)!,
           burnForBaseAction(pool.contract, account!, minRatio, maxRatio)!,
         ],
@@ -82,19 +64,12 @@ export const useRemoveLiquidity = (pool: IPool, input: string, method: RemoveLiq
           ignoreIf: alreadyApproved,
         },
       ]);
-      const [, , , deadline, v, r, s] = permits[0].args!;
+
+      const [, , , deadline, v, r, s] = permits[0].args! as LadleActions.Args.FORWARD_PERMIT;
 
       return batch(
         [
-          forwardPermitAction(
-            pool.address,
-            ladleContract?.address!,
-            _input,
-            deadline as BigNumberish,
-            v as BigNumberish,
-            r as Buffer,
-            s as Buffer
-          )!,
+          forwardPermitAction(pool.address, ladleContract?.address!, _input, deadline, v, r, s)!,
           transferAction(pool.address, pool.address, _input)!,
           burnAction(pool.contract, account!, account!, minRatio, maxRatio)!,
         ],
