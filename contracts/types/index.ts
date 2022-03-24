083a330b7518d4278044bd9f772147a124fe79ac
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-24 10:13:38 -0400

    update contracts with wrap ether module

--- contracts/types/index.ts
+++ contracts/types/index.ts
@@ -6,9 +6,11 @@ export type { ERC20Permit } from "./ERC20Permit";
 export type { FYToken } from "./FYToken";
 export type { Ladle } from "./Ladle";
 export type { Pool } from "./Pool";
+export type { WrapEtherModule } from "./WrapEtherModule";
 
 export { Cauldron__factory } from "./factories/Cauldron__factory";
 export { ERC20Permit__factory } from "./factories/ERC20Permit__factory";
 export { FYToken__factory } from "./factories/FYToken__factory";
 export { Ladle__factory } from "./factories/Ladle__factory";
 export { Pool__factory } from "./factories/Pool__factory";
+export { WrapEtherModule__factory } from "./factories/WrapEtherModule__factory";
