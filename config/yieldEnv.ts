5a2a806a8af76291d74be25d5d631a6e5e6a0c18
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-25 08:12:43 -0400

    remove unnecessary cauldron

--- config/yieldEnv.ts
+++ config/yieldEnv.ts
@@ -1,21 +1,17 @@
 const yieldEnv = {
   addresses: {
     1: {
-      Cauldron: '0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867',
       Ladle: '0x6cB18fF2A33e981D1e38A663Ca056c0a5265066A',
       WrapEtherModule: '0x22768FCaFe7BB9F03e31cb49823d1Ece30C0b8eA',
     },
     4: {
-      Cauldron: '0x040803C41fE50aDdda170b2d3facF7F1F7FF0855',
       Ladle: '0xc5A458D816D26f57750940992ae9D85af23B86A7',
       WrapEtherModule: '0xd6AdA52c4A04895c3Ef4668a1defd186ccD5aC44',
     },
     42: {
-      Cauldron: '0x82Dfaef826C8c190E9b472896a9a99165Ccee35a',
       Ladle: '0xe7bae0445B9a1DBE834a7379fbE23C6d2Bd61C59',
     },
     421611: {
-      Cauldron: '0x5DcC4152b5b685e9B094705f36D672Ae5D04134f',
       Ladle: '0xfdf43681C87Fcb0F9B601902CC668D7E6291Ce41',
     },
   },
