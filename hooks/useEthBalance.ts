3a80596e11a921247047434162ea8a8afbe80c44
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-25 08:12:35 -0400

    swr-like eth bal fetch

--- hooks/useEthBalance.ts
+++ hooks/useEthBalance.ts
@@ -3,12 +3,12 @@ import useSWR from 'swr';
 import useConnector from './useConnector';
 
 function useETHBalance() {
-  const { account, provider } = useConnector();
+  const { account, provider, chainId } = useConnector();
 
   const _getBalance = async () =>
     provider && account ? ethers.utils.formatEther(await provider.getBalance(account)) : '0';
 
-  const { data } = useSWR('/ethBalance', _getBalance);
+  const { data } = useSWR(`/ethBalance/${chainId}/${account}`, _getBalance);
 
   return { balance: data };
 }
