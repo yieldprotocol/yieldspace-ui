7ea11deba8de7ce115764ebcd9cdf1bed77edbfb
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-26 19:19:25 -0400

    slippage setting component

--- components/common/SlippageSetting.tsx
+++ components/common/SlippageSetting.tsx
@@ -1,18 +1,27 @@
-import { Menu, Transition } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import { CogIcon } from '@heroicons/react/outline';
 import { Fragment, useState } from 'react';
 import tw from 'tailwind-styled-components';
 
-const Input = tw.input`caret-gray-800 dark:caret-gray-50 text-md appearance-none dark:bg-gray-800 bg-gray-300 dark:focus:text-gray-50 focus:text-gray-800 dark:text-gray-300 text-gray-800 py-1 px-4 leading-tight focus:outline-none `;
+type ButtonProps = {
+  $active: boolean;
+};
 
-const SlippageSetting = ({ slippageTolerance }: { slippageTolerance: string }) => {
-  const [setting, setSetting] = useState<string>((+slippageTolerance * 100).toString()); // settings comes in as .005 for .5%
+const Input = tw.input`text-right caret-gray-800 dark:caret-gray-50 text-md appearance-none dark:bg-gray-800 bg-gray-300 dark:focus:text-gray-50 focus:text-gray-800 dark:text-gray-300 text-gray-800 leading-tight focus:outline-none `;
+const InputWrap = tw.div<ButtonProps>`${(p) =>
+  p.$active
+    ? 'border-primary-500 hover:border-primary-500/70'
+    : 'border-gray-600 hover:border-gray-500'} rounded-full flex px-2 py-1 border-[1px] items-center`;
+const Button = tw(InputWrap)`${(p) => p.$active && 'bg-primary-500'} cursor-pointer`;
+
+const SlippageSetting = () => {
+  const [setting, setSetting] = useState<string>(''); // settings comes in as .005 for .5%
 
   return (
-    <Menu as="div" className="relative">
-      <Menu.Button as="div" className="items-center cursor-pointer">
+    <Popover className="relative">
+      <Popover.Button as="div" className="items-center cursor-pointer">
         <CogIcon className="w-5 h-5 dark:text-gray-200 text-gray-800" aria-hidden="true" />
-      </Menu.Button>
+      </Popover.Button>
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
@@ -22,24 +31,30 @@ const SlippageSetting = ({ slippageTolerance }: { slippageTolerance: string }) =
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute w-40 right-0 origin-top-right rounded-lg shadow-lg z-50 dark:bg-gray-800 bg-gray-300 p-1">
-          <div className="p-1">
-            <Menu.Item as="div" className="p-1">
-              <div className="text-gray-400 text-sm">Slippage Tolerance</div>
-              <Input
-                name="setting"
-                type="number"
-                inputMode="decimal"
-                value={setting}
-                placeholder={setting}
-                onChange={(e) => setSetting(e.target.value)}
-                min="0"
-              />
-            </Menu.Item>
+        <Popover.Panel className="mt-2 absolute right-0 origin-top-right rounded-lg shadow-lg z-50 dark:bg-gray-800 bg-gray-300 p-5 border-[1px] border-gray-700">
+          <div className="grid gap-3">
+            <div className="text-gray-400 text-sm text-left">Slippage Tolerance</div>
+            <div className="flex justify-end gap-2">
+              <Button $active={setting === ''} onClick={() => setSetting('')}>
+                Auto
+              </Button>
+              <InputWrap $active={setting !== ''}>
+                <Input
+                  name="setting"
+                  type="number"
+                  inputMode="decimal"
+                  value={setting}
+                  placeholder=".5"
+                  onChange={(e) => setSetting(e.target.value)}
+                  min="0"
+                />
+                <span className="dark:text-gray-200 ml-[1px]">%</span>
+              </InputWrap>
+            </div>
           </div>
-        </Menu.Items>
+        </Popover.Panel>
       </Transition>
-    </Menu>
+    </Popover>
   );
 };
 
