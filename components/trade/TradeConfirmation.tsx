685e48dfeb6d27c390601227fb233a043a10e481
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-28 12:34:16 -0400

    use slip tol from localstorage

--- components/trade/TradeConfirmation.tsx
+++ components/trade/TradeConfirmation.tsx
@@ -6,9 +6,10 @@ import Button from '../common/Button';
 import useTimeTillMaturity from '../../hooks/useTimeTillMaturity';
 import InfoIcon from '../common/InfoIcon';
 import { ITradeForm } from './TradeWidget';
-import { cleanValue, valueAtDigits } from '../../utils/appUtils';
+import { valueAtDigits } from '../../utils/appUtils';
 import { calculateSlippage } from '../../utils/yieldMath';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { DEFAULT_SLIPPAGE, SLIPPAGE_KEY } from '../common/SlippageSetting';
 
 const Container = tw.div`relative flex justify-center items-center w-full`;
 const Wrap = tw.div`w-full text-center text-lg align-middle items-center`;
@@ -47,7 +48,7 @@ const ConfirmItem = ({ value, asset, pool }: { value: string; asset: IAsset; poo
 );
 
 const TradeConfirmation = ({ form, interestRate, action, disabled, loading }: ITradeConfirmation) => {
-  const [slippageTolerance] = useLocalStorage('slippageTolerance', '.005');
+  const [slippageTolerance] = useLocalStorage(SLIPPAGE_KEY, DEFAULT_SLIPPAGE);
   const { pool, fromAmount, fromAsset, toAmount, toAsset } = form;
   const timeTillMaturity_ = useTimeTillMaturity(pool?.maturity!);
   const fromAmount_ = valueAtDigits(fromAmount, fromAsset?.digitFormat!);
