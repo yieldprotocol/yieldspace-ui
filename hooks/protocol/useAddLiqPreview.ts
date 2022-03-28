89762f6f8315cfc13de712dab10e5e39f3a4e039
marcomariscal <marco.a.mariscal@gmail.com>
2022-03-28 10:53:21 -0400

    refactor checks

--- hooks/protocol/useAddLiqPreview.ts
+++ hooks/protocol/useAddLiqPreview.ts
@@ -3,54 +3,64 @@ import { useEffect, useState } from 'react';
 import { AddLiquidityActions } from '../../lib/protocol/liquidity/types';
 import { IPool } from '../../lib/protocol/types';
 import { fyTokenForMint, mint, mintWithBase, splitLiquidity } from '../../utils/yieldMath';
+import { useLocalStorage } from '../useLocalStorage';
 
-const useAddLiqPreview = (pool: IPool, baseAmount: string, method: AddLiquidityActions, slippageTolerance = 0.001) => {
+const useAddLiqPreview = (pool: IPool, baseAmount: string, method: AddLiquidityActions | undefined) => {
   const [lpTokenPreview, setLpTokenPreview] = useState<string>();
   const [fyTokenNeeded, setFyTokenNeeded] = useState<string>();
+  const [slippageTolerance] = useLocalStorage('slippageTolerance', '.005');
 
   useEffect(() => {
     (async () => {
-      if (!pool) {
-        return setLpTokenPreview('');
+      if (pool && baseAmount !== '' && method) {
+        const { totalSupply, decimals, contract, getTimeTillMaturity, ts, g1 } = pool;
+        const timeTillMaturity = getTimeTillMaturity().toString();
+
+        const _baseAmount = ethers.utils.parseUnits(baseAmount || '0', decimals);
+        const [cachedBaseReserves, cachedFyTokenReserves] = await contract.getCache();
+        const cachedRealReserves = cachedFyTokenReserves.sub(totalSupply);
+
+        // if minting with both base and fyToken, calculate how much fyToken is needed
+        if (method === AddLiquidityActions.MINT) {
+          const [, _fyTokenNeeded] = splitLiquidity(cachedBaseReserves, cachedRealReserves, _baseAmount);
+          setFyTokenNeeded(ethers.utils.formatUnits(_fyTokenNeeded, decimals));
+
+          const [minted] = mint(
+            cachedBaseReserves,
+            cachedRealReserves,
+            totalSupply,
+            BigNumber.from(_fyTokenNeeded),
+            false
+          );
+          setLpTokenPreview(ethers.utils.formatUnits(minted, decimals));
+        } else {
+          // minting with base
+          const [fyTokenToBuy] = fyTokenForMint(
+            cachedBaseReserves,
+            cachedRealReserves,
+            cachedFyTokenReserves,
+            _baseAmount,
+            timeTillMaturity,
+            ts,
+            g1,
+            decimals,
+            +slippageTolerance
+          );
+
+          const [minted] = mintWithBase(
+            cachedBaseReserves,
+            cachedFyTokenReserves,
+            cachedRealReserves,
+            fyTokenToBuy,
+            timeTillMaturity,
+            ts,
+            g1,
+            decimals
+          );
+
+          setLpTokenPreview(ethers.utils.formatUnits(minted, decimals));
+        }
       }
-      const { totalSupply, decimals, contract, getTimeTillMaturity, ts, g1 } = pool;
-      const timeTillMaturity = getTimeTillMaturity().toString();
-
-      const _baseAmount = ethers.utils.parseUnits(baseAmount || '0', decimals);
-      const [cachedBaseReserves, cachedFyTokenReserves] = await contract.getCache();
-      const cachedRealReserves = cachedFyTokenReserves.sub(totalSupply);
-
-      // if minting with both base and fyToken, calculate how much fyToken is needed
-      const [, _fyTokenNeeded] = splitLiquidity(cachedBaseReserves, cachedRealReserves, _baseAmount);
-      setFyTokenNeeded(ethers.utils.formatUnits(_fyTokenNeeded, decimals));
-
-      const [_fyTokenToBuy] = fyTokenForMint(
-        cachedBaseReserves,
-        cachedRealReserves,
-        cachedFyTokenReserves,
-        _baseAmount,
-        timeTillMaturity,
-        ts,
-        g1,
-        decimals,
-        slippageTolerance
-      );
-
-      const [minted] =
-        method === AddLiquidityActions.MINT_WITH_BASE
-          ? mintWithBase(
-              cachedBaseReserves,
-              cachedFyTokenReserves,
-              cachedRealReserves,
-              _fyTokenToBuy,
-              timeTillMaturity,
-              ts,
-              g1,
-              decimals
-            )
-          : mint(cachedBaseReserves, cachedRealReserves, totalSupply, BigNumber.from(_fyTokenNeeded), false);
-
-      setLpTokenPreview(ethers.utils.formatUnits(minted, decimals));
     })();
   }, [baseAmount, method, pool, slippageTolerance]);
 
