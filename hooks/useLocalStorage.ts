2bd3878bbf03dc9570e9923512a2f0fb27dceda4
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-04 13:49:01 -0400

    fix logic for getting value from localStorage

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 
-const getStorageValue = (key: string, defaultValue: string, account?: string) =>
-  typeof window !== 'undefined' ? JSON.parse(localStorage.getItem(key)!) : defaultValue;
+const getStorageValue = (key: string, defaultValue: string, account?: string) => {
+  const itemInLocalStorage: string | null = JSON.parse(localStorage.getItem(key)!);
+  return itemInLocalStorage ?? defaultValue;
+};
 
 export const useLocalStorage = (key: string, defaultValue: string) => {
   const [value, setValue] = useState<string>(() => getStorageValue(key, defaultValue));
