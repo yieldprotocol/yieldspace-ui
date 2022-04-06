eae57e30fd6e93408b62d6d056edc32bdba4a4c1
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-06 07:58:05 -0400

    spacing

--- components/styles/Header.tsx
+++ components/styles/Header.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import tw from 'tailwind-styled-components';
-const Wrap = tw.div`text-lg font-bold text-center justify-items-start align-middle dark:text-gray-50`;
+const Wrap = tw.div`text-lg font-bold text-center justify-items-start align-middle dark:text-gray-50 whitespace-nowrap`;
 
 const Header: FC = ({ children }) => <Wrap>{children}</Wrap>;
 
