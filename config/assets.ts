8fbb5a722bbf094f251215ef59cf2f051e2e1ffa
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-08 20:08:16 -0400

    style

--- config/assets.ts
+++ config/assets.ts
@@ -1,10 +1,3 @@
-import { ethers } from 'ethers';
-
-// base id's
-export const WETH = '0x303000000000';
-export const DAI = '0x303100000000';
-export const USDC = '0x303200000000';
-
-export const ETH_BASED_ASSETS = ['WETH', 'ETH', WETH, ethers.utils.formatBytes32String('ETH').slice(0, 14)];
-export const DAI_PERMIT_ASSETS = ['DAI', DAI];
-export const NON_PERMIT_ASSETS = ['ETH', 'WETH', WETH];
+export const ETH_BASED_ASSETS = ['WETH', 'ETH'];
+export const DAI_PERMIT_ASSETS = ['DAI'];
+export const NON_PERMIT_ASSETS = ['ETH', 'WETH'];
