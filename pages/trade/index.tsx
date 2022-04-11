ed8fbdf9b345a940abce3a9608e19bb08be105e0
marcomariscal <marco.a.mariscal@gmail.com>
2022-04-11 13:34:22 -0400

    try to catch error

--- pages/trade/index.tsx
+++ pages/trade/index.tsx
@@ -1,20 +1,24 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { InferGetServerSidePropsType } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import TradeWidget from '../../components/trade/TradeWidget';
 import { URLS } from '../../config/chains';
 import { IPoolMap } from '../../lib/protocol/types';
 import { getContracts, getPools } from '../../lib/protocol';
 
-const Trade = ({ pools }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
-  <TradeWidget pools={JSON.parse(pools) as IPoolMap} />
+const Trade = ({ pools }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <TradeWidget pools={JSON.parse(pools!) as IPoolMap} />
 );
 
 export default Trade;
 
-export async function getServerSideProps(context) {
-  const chainId = context.query.chainId || 4;
-  const provider = new JsonRpcProvider(URLS[chainId][0], chainId);
-  const contracts = getContracts(provider, chainId);
-  const pools = await getPools(provider, contracts!, chainId, undefined);
-  return { props: { pools: JSON.stringify(pools) } };
+export async function getStaticProps() {
+  try {
+    const chainId = 4;
+    const provider = new JsonRpcProvider(URLS[chainId][0], chainId);
+    const contracts = getContracts(provider, chainId);
+    const pools = await getPools(provider, contracts!, chainId, undefined);
+    return { props: { pools: JSON.stringify(pools) } };
+  } catch (error) {
+    return { notFound: true, props: { pools: undefined } };
+  }
 }
