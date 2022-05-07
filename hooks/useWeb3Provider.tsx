eec0d71850d8e8231312c4ac464eb9b5f35db966
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-07 08:56:27 -0400

    rainbow theme

--- hooks/useWeb3Provider.tsx
+++ hooks/useWeb3Provider.tsx
@@ -1,8 +1,15 @@
-import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit';
+import {
+  apiProvider,
+  configureChains,
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+  Theme,
+} from '@rainbow-me/rainbowkit';
 import { chain, createClient, WagmiProvider } from 'wagmi';
+import merge from 'lodash.merge';
 
 export default function Web3Provider({ children }) {
-  const theme = useappsele;
   const { chains, provider } = configureChains(
     [chain.mainnet, chain.goerli, chain.rinkeby, chain.arbitrum, chain.arbitrumRinkeby],
     [
@@ -22,22 +29,19 @@ export default function Web3Provider({ children }) {
     provider,
   });
 
-  const myTheme = merge(darkTheme(), {
+  const theme = merge(darkTheme(), {
     colors: {
       accentColor:
         'linear-gradient(135deg, rgba(247, 149, 51, 0.5), rgba(243, 112, 85, 0.5), rgba(239, 78, 123, 0.5), rgba(161, 102, 171, 0.5), rgba(80, 115, 184, 0.5), rgba(16, 152, 173, 0.5), rgba(7, 179, 155, 0.5), rgba(111, 186, 130, 0.5));',
     },
+    fonts: { body: 'inter' },
   } as Theme);
-
-  const theme: Theme = {
-    colors: {
-      accentColor: '',
-    },
-  };
 
   return (
     <WagmiProvider client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+      <RainbowKitProvider chains={chains} theme={theme}>
+        {children}
+      </RainbowKitProvider>
     </WagmiProvider>
   );
 }
