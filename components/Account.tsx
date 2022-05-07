58348a2541c0d3332ad1a5adac1f303233c36368
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-07 08:48:46 -0400

    install lodash for rainbow themeing

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,30 +1,9 @@
-import { useState } from 'react';
-import tw from 'tailwind-styled-components';
-import ConnectModal from './ConnectModal';
-import Dropdown from './ConnectDropdown';
-import Chain from './Chain';
-import ETHBalance from './common/ETHBalance';
-import { useAccount } from 'wagmi';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-const ConnectButton = tw.button`bg-primary-500/25 align-middle px-4 py-2 text-primary-500 rounded-md hover:bg-primary-600/25`;
-
-const Account = () => {
-  const account = useAccount();
-  const [connectModalOpen, setConnectModalOpen] = useState<boolean>(false);
-  return (
-    <div className="flex justify-end">
-      {!account ? (
-        <ConnectButton onClick={() => setConnectModalOpen(true)}>Connect Wallet</ConnectButton>
-      ) : (
-        <div className="flex gap-3">
-          <Chain />
-          <ETHBalance />
-          <Dropdown setModalOpen={setConnectModalOpen} />
-        </div>
-      )}
-      {connectModalOpen && <ConnectModal modalOpen={connectModalOpen} setModalOpen={setConnectModalOpen} />}
-    </div>
-  );
-};
+const Account = () => (
+  <div className="flex justify-end">
+    <ConnectButton accountStatus="full" />
+  </div>
+);
 
 export default Account;
--- hooks/useWeb3Provider.tsx
+++ hooks/useWeb3Provider.tsx
@@ -1,7 +1,8 @@
-import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { apiProvider, configureChains, getDefaultWallets, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit';
 import { chain, createClient, WagmiProvider } from 'wagmi';
 
 export default function Web3Provider({ children }) {
+  const theme = useappsele;
   const { chains, provider } = configureChains(
     [chain.mainnet, chain.goerli, chain.rinkeby, chain.arbitrum, chain.arbitrumRinkeby],
     [
@@ -20,6 +21,19 @@ export default function Web3Provider({ children }) {
     connectors,
     provider,
   });
+
+  const myTheme = merge(darkTheme(), {
+    colors: {
+      accentColor:
+        'linear-gradient(135deg, rgba(247, 149, 51, 0.5), rgba(243, 112, 85, 0.5), rgba(239, 78, 123, 0.5), rgba(161, 102, 171, 0.5), rgba(80, 115, 184, 0.5), rgba(16, 152, 173, 0.5), rgba(7, 179, 155, 0.5), rgba(111, 186, 130, 0.5));',
+    },
+  } as Theme);
+
+  const theme: Theme = {
+    colors: {
+      accentColor: '',
+    },
+  };
 
   return (
     <WagmiProvider client={wagmiClient}>
