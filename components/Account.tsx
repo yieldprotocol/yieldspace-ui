4fc2e0eb1208cc68413bd1c5f70cd40374df1d86
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-08 18:25:28 -0400

    use rainbow connect button

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,8 +1,10 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import InfoDropdown from './InfoDropdown';
 
 const Account = () => (
-  <div className="flex justify-end">
+  <div className="flex justify-end items-center">
     <ConnectButton accountStatus="full" />
+    {/* <InfoDropdown /> */}
   </div>
 );
 
