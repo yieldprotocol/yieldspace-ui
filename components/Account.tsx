2adbb3c41bd2eac2aefd398567e308686a55c9d8
marcomariscal <marco.a.mariscal@gmail.com>
2022-05-09 15:12:54 -0400

    comment unused

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,5 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import InfoDropdown from './InfoDropdown';
+// import InfoDropdown from './InfoDropdown';
 
 const Account = () => (
   <div className="flex justify-end items-center">
